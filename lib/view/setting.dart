7cb19ed509b8ad3f37880a8e4b99449458fdb246
NaufalArig <naufal16.na@gmail.com>
2023-10-23 14:02:45 +0700

    update setting

--- lib/view/setting.dart
+++ lib/view/setting.dart
@@ -5,38 +5,51 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Setelan'),
+        title: const Text('Setelan'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Lokasi',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
             ListTile(
-              leading: Icon(Icons.language),
+              title: Text('Lokasi'),
+              subtitle: Text('Ubah lokasi saat ini'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {},
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Bahasa',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+            ListTile(
               title: Text('Ganti Bahasa'),
+              subtitle: Text('Ubah bahasa aplikasi'),
+              trailing: Icon(Icons.arrow_forward_ios),
               onTap: () {
-                // Tambahkan logika ketika opsi "Ganti Bahasa" diklik.
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return BahasaPage();
+                }));
               },
             ),
-            ListTile(
-              leading: Icon(Icons.notifications),
-              title: Text('Notifikasi'),
-              onTap: () {
-                // Tambahkan logika ketika opsi "Tentang" diklik.
-              },
+            SizedBox(height: 16),
+            Text(
+              'Tentang Aplikasi',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
             ),
             ListTile(
-              leading: Icon(Icons.contact_phone),
-              title: Text('Hubungi Kami'),
-              onTap: () {
-                // Tambahkan logika ketika opsi "Hubungi Kami" diklik.
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.info),
               title: Text('Tentang'),
+              subtitle: Text('Informasi tentang aplikasi'),
+              trailing: Icon(Icons.arrow_forward_ios),
               onTap: () {
-                // Tambahkan logika ketika opsi "Tentang" diklik.
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return AboutPage();
+                }));
               },
             ),
           ],
@@ -45,3 +58,77 @@ class SettingsPage extends StatelessWidget {
     );
   }
 }
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tentang Aplikasi'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('AIR-QU',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 8),
+            Text('Versi: 1.0.0'),
+            Text('Pengembang: -'),
+            Text('Email: developer@gmail.com'),
+            SizedBox(height: 16),
+            Text('Tentang Aplikasi',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 8),
+            Text('.............................'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BahasaPage extends StatefulWidget {
+  @override
+  _BahasaPageState createState() => _BahasaPageState();
+}
+class _BahasaPageState extends State<BahasaPage> {
+   int selectedLanguage = 0;
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ganti Bahasa'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Bahasa Indonesia'),
+            leading: selectedLanguage == 0
+                ? Icon(Icons.check, color: Colors.green)
+                : SizedBox(width: 24,), // Tampilkan ceklis jika dipilih
+            onTap: () {
+              setState(() {
+                selectedLanguage = 0;
+              });
+            },
+          ),
+          ListTile(
+            title: Text('Bahasa Inggris'),
+            leading: selectedLanguage == 1
+                ? Icon(Icons.check, color: Colors.green)
+                : SizedBox(width: 24), // Tampilkan ceklis jika dipilih
+            onTap: () {
+              setState(() {
+                selectedLanguage = 1;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
